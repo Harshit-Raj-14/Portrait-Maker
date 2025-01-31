3324416ba73e7ce928c80d164dee773377395b33
Harshit Raj <harshitintelligent@gmail.com>
2025-01-31 13:03:37 +0530

    hi

--- frontend/app/dashboard/_components/header.jsx
+++ frontend/app/dashboard/_components/header.jsx
@@ -7,7 +7,7 @@ function Header() {
   return (
     <div className='p-3 px-5 flex items-center justify-between shadow-md'>
         <div className='flex gap-3 items-center'>
-            <Image src={'logo.svg'} width={30} height={30} alt='Logo' />
+            <Image src={'/logo.svg'} width={30} height={30} alt='Logo' />
             <h2 className='font-bold text-xl'>Live Portrait Maker</h2>
         </div>
         <div className='flex gap-3 items-center'>
